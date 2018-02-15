7988c3ac9370a29d224047c80c7373c33ec47ce8
Fabio Hirt <fabio_hirt@outlook.com>
2018-02-15 21:01:29 +0100

    Finished Instagram Picture Display

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # node-basic
+This basic node app displays a users recent instagram pictures. It is based on a tutorial from https://scotch.io I used this app to learn about several topics:
+1. create a http server with express as a framework
+2. use the instagram-node package to access the instagram developer API
+3. use the EJS templating engine
+4. use the dotenv package to hide a secret access token
--- server.js
+++ server.js
@@ -1,12 +1,40 @@
-// setup the app
+// IMPORTS
+// =======================================================
 var express = require('express');
 var app = express();
+var ig = require('instagram-node').instagram();
+require('dotenv').config();
 
-// define routes
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+// CONFIGURE THE APP
+// =======================================================
+// static resources
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine
+app.set('view engine', 'ejs');
+
+// configure instagram access
+ig.use({
+  access_token: process.env.INSTA_TOKEN
 });
 
-// start the server on port 8080
-app.listen(8080);
-console.log('Server has started!')
+// SETUP ROUTES
+// =======================================================
+app.get('/', function(req, res) {
+  // get popular media from instagram
+  ig.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
+    if(err) {
+      // TODO: redirect to error handling
+      console.log(err.code);
+      console.log(err.message);
+      return 0;
+    }
+    
+    res.render('pages/index', { grams: medias });
+  });
+});
+
+// START THE SERVER
+// =======================================================
+app.listen(process.env.PORT);
+console.log('Server started on port:' + process.env.PORT);
